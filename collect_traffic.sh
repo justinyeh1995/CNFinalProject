ee667ac536d526576447b5fad63452b7c9e68274
justinyeh1995 <justinyeh1995@gmail.com>
2022-12-15 08:31:00 +0000

    update collection

--- collect_traffic.sh
+++ collect_traffic.sh
@@ -12,25 +12,26 @@ do
         bytes=$(awk -F "," '{split($5,b,"="); print b[2]","}' data/flowentries.csv)
         ipsrc=$(awk -F "," '{out=""; for(k=2;k<=NF;k++){out=out" "$k}; print out}' data/flowentries.csv | awk -F " " '{split($11,d,"="); print d[2]","}')
         ipdst=$(awk -F "," '{out=""; for(k=2;k<=NF;k++){out=out" "$k}; print out}' data/flowentries.csv | awk -F " " '{split($12,d,"="); print d[2]","}')
-        # check if there are no traffics in the network at the moment.
+
+        # check if all the strings are of length 0; then there are no traffics in the network at the moment.
         if test -z "$packets" || test -z "$bytes" || test -z "$ipsrc" || test -z "$ipdst" 
         then
             state=0
+        # if traffics detected then start inspect the situation.
         else
             echo "$packets" > data/packets.csv
             echo "$bytes" > data/bytes.csv
             echo "$ipsrc" > data/ipsrc.csv
             echo "$ipdst" > data/ipdst.csv
-            
+        
             python3 computeRTData.py
             python3 inspector.py
             state=$(awk '{print $0;}' .result)
-        fi
+	fi
 
         if [ $state -eq 1 ];
         then
             echo "Network is under attack occuring at s$j"
-            #
             default_flow=$(sudo ovs-ofctl dump-flows s$j | tail -n 1)    # Get flow "action:CONTROLLER:<port_num>" sending unknown packet to the controller
             sudo ovs-ofctl del-flows s$j
             sudo ovs-ofctl add-flow s$j "$default_flow"
